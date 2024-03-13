45873bcbabf900053539f940513c631e521a7545
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2024-03-13 17:31:19 +0600

    updated

---
+++ MERN Stack  Web Development/Backend/NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+
+var name = superheroes.random();
+
+console.log(`My Favourite superhero ${name}`);
