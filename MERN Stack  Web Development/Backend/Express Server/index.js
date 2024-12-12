e0c1ec4ab11a90977d3909a1312dce20ed68febe
mridul <mridulroy010@gmail.com>
2024-12-12 12:02:27 +0600

    Updated

--- MERN Stack  Web Development/Backend/Express Server/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-const port = 1000;
-
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-})
--- MERN Stack  Web Development/jQurey/index.js
+++
@@ -1,2 +0,0 @@
-$("body").addClass("bg_color");
-$("button").addClass("btn_style");
