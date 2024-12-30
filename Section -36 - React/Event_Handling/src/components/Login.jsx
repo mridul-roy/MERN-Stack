76fe746333c4a87d35a52adacdeb830859f69438
mridul <mridulroy010@gmail.com>
2024-12-30 23:44:07 +0600

    Event_handeled

---
+++ Section -36 - React/Event_Handling/src/components/Login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login;
