6b1bd79877d73f2c1c47d334698b6c1b9cc54db9
mridul <mridulroy010@gmail.com>
2025-01-01 23:36:53 +0600

    Updated

--- Section -36 - React/Event_Handling/src/App.jsx
+++ Section -36 - React/Event_Handling/src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css'
-import Login from './components/Login'
+import {useState} from "react";
 
 function App() {
+const [headingText, setHeadingText] = useState("Hello");
+
+const [isMouseOver, setMouseOver] = useState(false);
+
+
+function handleClick(){
+    setHeadingText("Submitted");
+};
+
+
+function handleMouseOver(){
+  setMouseOver(true)
+    
+    };  
+
+
+
   return (
     <div>
-      <Login />
+      <h1>Login</h1>
+      <h2>{headingText}</h2>
+      <input type="text" placeholder="Username"/>
+      <input type="password" placeholder="Password"/>
+      <button onClick={handleClick} onMouseOver={handleMouseOver} style={{backgroundColor: isMouseOver? "green" : "white"}}>Login</button>
 
     </div>
   )
 }
 
-export default App
+export default App;
