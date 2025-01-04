f579eebb9fdf2f327acfbba5c6faa196ad2ea321
mridul <mridulroy010@gmail.com>
2025-01-04 22:51:17 +0600

    From added

---
+++ Section -36 - React/React_From_Ex/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+
+  function handleTextOver(event){
+    setHeadingText(event.target.value)
+  }
+ 
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <h2>{headingText}</h2>
+      <input onChange={handleTextOver} type="text" placeholder="Username"/>
+      <input type="password" placeholder="Password"/>
+      <button>Login</button>
+      
+    </div>
+  )
+}
+
+export default App
